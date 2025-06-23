7627d89c5c6b8899da68af603d906e5a560cca8e
糖糖 <yx_alkaid@qq.com>
2025-06-23 10:34:59 +0800

    refactor: 提取常量并优化主程序
    - 新增 Constant 类，用于存储常量值
    - 将主机名和资源路径提取为常量
    - 更新 Main 类，使用常量替代硬编码值
    - 简化 Main.main 方法，提高代码可读性

---
+++ src/main/java/com/alkaid/Constant.java
@@ -0,0 +1,10 @@
+package com.alkaid;
+
+public class Constant {
+
+    public static final String HOST="Speedwayr-11-25-ab.local";
+
+    public static final String RESOURCE_PATH="./src/main/resources";
+
+    
+}
--- src/main/java/com/alkaid/Main.java
+++ src/main/java/com/alkaid/Main.java
@@ -10,19 +10,16 @@ import lombok.extern.log4j.Log4j;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.print("Must pass reader hostname or IP as agument 1");
-        }
 
         // 资源文件目录
-        final String RESOURCE_PATH="./src/main/resources";
+        final String RESOURCE_PATH=Constant.RESOURCE_PATH;
 
         // BasicConfigurator.configure();
         PropertyConfigurator.configure(Path.of(RESOURCE_PATH, "log4j.properties").toString());
 
         LogReader reader = new LogReader("./output");
         reader.initialize(
-            args[0], 
+            Constant.HOST, 
         Path.of(RESOURCE_PATH, "SET_READER_CONFIG.xml").toString(), 
         Path.of(RESOURCE_PATH, "ADD_ROSPEC.xml").toString()
         );
